71477146622bfff92c6b4198e1531818180524d1
nithinmudhiraj7799 <talarinithin7799@gmail.com>
2025-05-24 17:24:34 +0530

    path corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import Signup from './components/signup';
-import Login from './components/login';
-import Home from './components/home';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/login.jsx';
+import Signup from './components/signup.jsx'; 
+import Home from './components/home.jsx';
 
 function App() {
   return (
-    <Routes>
-        <Route path="/Home" element={<Home/>} />
-      <Route path="/" element={<Signup/>} />
-      <Route path="/login" element={<Login/>} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
